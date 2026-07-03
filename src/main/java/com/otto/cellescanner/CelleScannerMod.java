5f7263947566a7ad66d99a8f504b5725bcf6393b
otto-BigO <skibidiotto@gmail.com>
2026-07-03 19:24:58 +0200

    release 1.1.2: persistent player model + celler list, ESP on everyone, troll sounds, item log, PvP mine, hub settings gear

--- src/main/java/com/otto/cellescanner/CelleScannerMod.java
+++ src/main/java/com/otto/cellescanner/CelleScannerMod.java
@@ -17,7 +17,7 @@ public class CelleScannerMod {
     // the display name is the new hub brand. See MassiveoAddons.
     public static final String MODID = "cellescanner";
     public static final String NAME = "Massiveo's Freaky Addons";
-    public static final String VERSION = "1.1.2-t13";
+    public static final String VERSION = "1.1.2";
 
     public static CelleConfig config;
     public static CelleScanner scanner;
